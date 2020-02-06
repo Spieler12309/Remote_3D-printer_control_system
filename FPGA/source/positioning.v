cb25ad4cc2805ab5958bd6a3d5cf3b12721b1626
Spieler <vvzunin@edu.hse.ru>
2020-02-06 16:41:56 +0300

    v0.26
    update find_max_timing

--- FPGA/source/positioning.v
+++ FPGA/source/positioning.v
@@ -4,38 +4,38 @@ module positioning (
 
 	//Управляющие сигналы двигателей
 	//Двигатель a
-	input						stepper_a_enable,
-	input						stepper_a_step,
-	input						stepper_a_direction,	
+	input		wire			stepper_a_enable,
+	input		wire			stepper_a_step,
+	input		wire			stepper_a_direction,	
 	
 	//Двигатель b
-	input						stepper_b_enable,
-	input						stepper_b_step,
-	input						stepper_b_direction,	
+	input		wire			stepper_b_enable,
+	input		wire			stepper_b_step,
+	input		wire			stepper_b_direction,	
 	
 	//Двигатель оси z
-	input						stepper_z_enable,
-	input						stepper_z_step,
-	input						stepper_z_direction,	
+	input		wire			stepper_z_enable,
+	input		wire			stepper_z_step,
+	input		wire			stepper_z_direction,	
 	
 	//Двигатель экструдера (подачи пластика)
-	input						stepper_e1_enable,
-	input						stepper_e1_step,
-	input						stepper_e1_direction,
+	input		wire			stepper_e1_enable,
+	input		wire			stepper_e1_step,
+	input		wire			stepper_e1_direction,
 
 
 	//Информация об инверсия двигателей
-	input						stepper_a_inversion,
-	input						stepper_b_inversion,
-	input						stepper_z_inversion,
-	input						stepper_e1_inversion,
+	input		wire			stepper_a_inversion,
+	input		wire			stepper_b_inversion,
+	input		wire			stepper_z_inversion,
+	input		wire			stepper_e1_inversion,
 
 
 	//Текущая позиция
-	output	reg		[31:0]	pos_x = 0,
-	output	reg		[31:0]	pos_y = 0,
-	output	reg		[31:0]	pos_z = 0,
-	output	reg		[31:0]	pos_e1 = 0
+	output	reg				[31:0]	pos_x = 0,
+	output	reg				[31:0]	pos_y = 0,
+	output	reg				[31:0]	pos_z = 0,
+	output	reg	signed	[31:0]	pos_e1 = 0
 );
 
 	always @(posedge stepper_a_step)
--- FPGA/source/speed-jerk-acc/find_max_timing.sv
+++ FPGA/source/speed-jerk-acc/find_max_timing.sv
@@ -14,7 +14,49 @@ module find_max_timing(
 
 
 	output	wire	[63:0]	max_timing 	[0:3],
+	output	wire	[31:0]	max_params [0:4],
 	output	reg				finish	
 	);
 
+always @(posedge clk)
+begin
+	if (reset)
+	begin
+		max_timing[0] <= 'd0;
+		max_timing[1] <= 'd0;
+		max_timing[2] <= 'd0;
+		max_timing[3] <= 'd0;
+		finish		  <= 'b0;
+	end
+	else
+	begin
+		if (timing_a[3] > timing_b[3] && timing_a[3] > timing_z[3] && timing_a[3] > timing_e[3])
+		begin
+			max_params = params_a;
+			max_timing = timing_a;
+		end
+		else
+		begin
+			if (timing_b[3] > timing_a[3] && timing_b[3] > timing_z[3] && timing_b[3] > timing_e[3])
+			begin
+				max_params = params_b;
+				max_timing = timing_b;
+			end
+			else
+			begin
+				if (timing_z[3] > timing_b[3] && timing_z[3] > timing_a[3] && timing_z[3] > timing_e[3])
+				begin
+					max_params = params_z;
+					max_timing = timing_z;
+				end
+				else
+				begin
+					max_params = params_e;
+					max_timing = timing_e;
+				end
+			end
+		end
+	end
+end
+	
 endmodule
